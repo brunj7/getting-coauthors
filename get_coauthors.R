d7c0883dfd2b7a3e0bb1792151c480e22f949209
Julien Brun <brunj7@users.noreply.github.com>
2024-10-09 16:58:32 -0700

    better comments, removed extras

--- get_coauthors.R
+++ get_coauthors.R
@@ -3,18 +3,16 @@ library(rcrossref)
 
 
 main_author <- "Brun, Julien"
-start_year <- "2022" 
-
-doi_example <- "10.1038/s41893-023-01253-y"
+# start_year <- "2022" 
+# 
+# doi_example <- "10.1038/s41893-023-01253-y"
 
 # Manually adding my papers from 2022 and on
 my_dois <- c("10.1038/s41893-023-01253-y", "10.1002/pan3.10524", "10.1111/gcb.16862",
              "10.1029/2022GB007580", "10.1088/1748-9326/acab19", "10.1371/journal.pcbi.1010567", "10.1002/ece3.9245")
 
 
-filter=list(has_funder=TRUE, has_full_text=TRUE)
-
-
+# function to get the co-authors of a specific paper
 get_paper_info <- function(doi_paper, main_author_l = main_author){
 
   # Get one paper
@@ -40,11 +38,13 @@ all_co <- data.frame(
   authors=character(),
   stringsAsFactors=FALSE)
 
+# Loop through the DOIs to get the co-authors
 for (paper in my_dois) {
   my_co <- get_paper_info(paper)
   all_co <- bind_rows(all_co, my_co)
 }
 
+# Save result as a csv file
 write_csv(all_co, "my_coauthors.csv")
 
 
